9c9dedd1f6e6a321a63d07baf242a3753bd655df
SeongUk <pkseonguk@gmail.com>
2023-05-21 06:51:41 +0900

    Fix 2

--- src/main/java/near/socket/GameService.java
+++ src/main/java/near/socket/GameService.java
@@ -23,7 +23,7 @@ public class GameService {
     private Map<String, GameRoom> gameRooms;
     private Set<Player> readyQueue;
 
-    static final int LIMIT = 4;
+    static final int LIMIT = 2;
     @Scheduled(cron = "0/3 * * * * ?")
     public void autoUpdate() throws Exception {
 
